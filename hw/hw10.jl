5af0b73af4a5a525d64033d9ead2a8ee08046ed6
Shao-Ting Chiu <r07945001@ntu.edu.tw>
2022-11-22 20:41:23 -0600

    bug: normal sigma

--- hw/hw10.jl
+++ hw/hw10.jl
@@ -18,39 +18,35 @@ k = 300000
 δ² = 1.
 θs = zeros(k)
 
-θs[1] = rand(θ)
-for i in 2:k 
-    J = Normal(θs[i-1], δ²)
+th = 0.
+for i in 1:k 
+    θᵒ = th
+    J = Normal(θᵒ, δ²)
     θʷ = rand(J)
-    θᵒ = θs[i-1]
-    r_log = sum(logpdf.( Normal(θʷ, 1), ys)) + logpdf(θ, θʷ) - sum(logpdf.( Normal(θᵒ, 1), ys)) - logpdf(θ, θᵒ)
+    r_log = sum(logpdf.( Normal(θʷ, 1), ys) .- logpdf.( Normal(θᵒ, 1), ys)) + logpdf(θ, θʷ) - logpdf(θ, θᵒ)
 
     # Accept
-    u_log = log(rand(Uniform(0,1)))
+    u_log = log(rand(Uniform()))
     if r_log > u_log 
-       θs[i] = θʷ
+        θs[i] = θʷ
     else 
         θs[i] = θᵒ
     end 
+    th = θs[i]
 end
 
 
-
-σ²ₙ = (1. + length(ys) * 1)^-1
-μₙ = (length(ys)* 1/ (σ²ₙ^-1)) * mean(ys)
-θ_exact = Normal(μₙ, σ²ₙ)
+# Exact PDF
+n = length(ys)
+τₙ =( 1 / (n/ 1 + 1/ 1)^0.5)
+μₙ = mean(ys) * (n / 1) / (n/ 1 + 1/1) + 0 
+θ_exact = Normal(μₙ, τₙ )
 xs = 0:0.01:4
 ys_exact = pdf.(θ_exact, xs)
 
-
-function exact(θ, ys)
-    p_log = -0.5 * θ^2 + (-0.5)* sum((ys .- θ).^2)
-    return exp(p_log)
-end
-
-xs = 0:0.00001:4
-ys_exact = [exact(x, ys) for x in xs]
-ys_exact = ys_exact 
-
-p = histogram(θs, normalize=true, xlabel="θ", ylabel="P(θ|y)", color="black", label="Metropolis")
+# Display
+p = histogram(θs, normalize=:pdf, xlabel="θ", ylabel="P(θ|y)", label="Metropolis", title="Normal Model")
 plot!(xs, ys_exact)
+
+"""
+"""
