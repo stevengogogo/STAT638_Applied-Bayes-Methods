e189d6db3365e1d3634928e777545e171ed53892
Shao-Ting Chiu <r07945001@ntu.edu.tw>
2022-11-22 21:21:52 -0600

    complete hw10

--- hw/hw10.jl
+++ hw/hw10.jl
@@ -10,31 +10,38 @@ using Statistics
 using Distributions
 using LinearAlgebra
 using Plots
+using KernelDensity
 
 ys = [-1., 0., 1., 10.]
+
+
 
 θ = Normal()
 k = 300000
 δ² = 1.
-θs = zeros(k)
 
-th = 0.
-for i in 1:k 
-    θᵒ = th
-    J = Normal(θᵒ, δ²)
-    θʷ = rand(J)
-    r_log = sum(logpdf.( Normal(θʷ, 1), ys) .- logpdf.( Normal(θᵒ, 1), ys)) + logpdf(θ, θʷ) - logpdf(θ, θᵒ)
+function sampling_ma(like_dist, θ, k, δ²)
+    θs = zeros(k)
+    th = 0.
+    for i in 1:k 
+        θᵒ = th
+        J = Normal(θᵒ, δ²)
+        θʷ = rand(J)
+        r_log = sum(logpdf.( like_dist(θʷ, 1), ys) .- logpdf.( like_dist(θᵒ, 1), ys)) + logpdf(θ, θʷ) - logpdf(θ, θᵒ)
 
-    # Accept
-    u_log = log(rand(Uniform()))
-    if r_log > u_log 
-        θs[i] = θʷ
-    else 
-        θs[i] = θᵒ
-    end 
-    th = θs[i]
+        # Accept
+        u_log = log(rand(Uniform()))
+        if r_log > u_log 
+            θs[i] = θʷ
+        else 
+            θs[i] = θᵒ
+        end 
+        th = θs[i]
+    end
+    return θs
 end
 
+θs_n = sampling_ma(Normal, θ, k, δ²)
 
 # Exact PDF
 n = length(ys)
@@ -46,7 +53,27 @@ ys_exact = pdf.(θ_exact, xs)
 
 # Display
 p = histogram(θs, normalize=:pdf, xlabel="θ", ylabel="P(θ|y)", label="Metropolis", title="Normal Model")
-plot!(xs, ys_exact)
+plot!(p, xs, ys_exact)
 
 """
+(2)
 """
+
+θs_c = sampling_ma(Cauchy, θ, k, δ²)
+p2 = histogram(θs, normalize=:pdf, xlabel="θ", ylabel="P(θ|y)", label="Metropolis", title="Cauchy Model")
+
+display(p2)
+
+
+"""
+(3)
+"""
+Un = kde(θs_n)
+Uc = kde(θs_c)
+pdf_n = [pdf(Un, x) for x in xs]
+pdf_c = [pdf(Uc, x) for x in xs]
+p3 = plot()
+plot!(p3, xs, pdf_n, label="Normal")
+plot!(p3, xs, pdf_c, label="Cauchy")
+plot!(p3, xs, ys_exact, label="Exact PDF (Normal)")
+display(p3)
